36689428fcbd70f47671b5c7d0947a51ae3d538b
sanya <79fz270704@gmail.com>
2019-12-10 15:36:28 +0300

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pprint import pprint
+import termtables as tt
 
 from entities import Bag, CardStack, WinTheGame
 
@@ -7,13 +7,20 @@ bag = Bag()
 stack = CardStack()
 
 card = stack.get_card()
-pprint(card.preview())
+print('Ваша карта')
+print(tt.to_string(
+    data=card.preview(),
+    header=[],
+    alignment='c',
+    style=tt.styles.rounded_double
+))
+
 step = 0
 while True:
     barrel = bag.get_barrel()
     step += 1
     print(f'\nШаг № {step}')
-    print(f'Из мешка вытянули {barrel}')
+    print(f'Из мешка вытянули бочонок с номером {barrel}')
     try:
         print('Есть на карте' if barrel in card else 'Нет на карте')
     except WinTheGame:
